013a44d304b78417eafb5fb4bba35fd34cf71919
marcos <dev.marcosanjos@gmail.com>
2025-01-31 23:16:06 -0300

    feat: add authentication module with signin functionality and DTO validation

---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,35 @@
+import { AuthenticateDto } from 'src/modules/auth/dtos/authenticate.dto';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserRepository } from 'src/shared/database/repositories/users.repositories';
+import { compare } from 'bcryptjs';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly usersRepo: UserRepository) {}
+
+  async authenticate(authenticateDto: AuthenticateDto) {
+    const { email, password } = authenticateDto;
+
+    const user = await this.usersRepo.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const isPasswordValid = await compare(password, user.password);
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    return { isPasswordValid };
+  }
+}
