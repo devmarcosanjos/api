6a72e6452b10334c9b3935e9b5212d47c630e987
marcos <dev.marcosanjos@gmail.com>
2025-02-02 12:26:20 -0300

    feat: configure global AuthGuard for enhanced route protection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,17 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from 'src/modules/users/users.module';
 import { DatabaseModule } from './shared/database/database.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from 'src/modules/auth/auth.guard';
 
 @Module({
   imports: [UsersModule, DatabaseModule, AuthModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,13 +1,11 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { AuthGuard } from 'src/modules/auth/auth.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('/me')
-  @UseGuards(AuthGuard)
   me(@Req() request: { userId: string }) {
     console.log({ me: request.userId });
     return this.usersService.getUserById('userId');
